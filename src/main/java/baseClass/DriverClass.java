72484e59437184711c9928e2186acb2897c5bd60
Rishabh Patil <rishabhpatil6@hotmail.com>
2018-12-20 11:23:44 +0530

    New

--- src/main/java/baseClass/DriverClass.java
+++ src/main/java/baseClass/DriverClass.java
@@ -19,7 +19,7 @@ public class DriverClass {
       caps.setCapability("automationName", "XCUITest");
       caps.setCapability("platformName", "iOS");
       //caps.setCapability("platformVersion", "11.4.1");
-      caps.setCapability("platformVersion", "12.1.1");
+      caps.setCapability("platformVersion", "12.1");
       //caps.setCapability("platformVersion", "12.0.1");
       caps.setCapability("deviceName","Iphone");
       //caps.setCapability("udid", "600cb9c46c56a6a37d1349373fc58f61cd022f72");				//IPHONE11
@@ -29,7 +29,8 @@ public class DriverClass {
       //caps.setCapability("udid", "096258d54e191bdd1a44efe7df3679a92fb5ecf9");				//IPHONE 7
       //caps.setCapability("udid", "e0de07129fa1b2b131462ce87a84c9c43c93e1da");				//IPHONE 13
       //caps.setCapability("udid", "70879e0bd5b245a55107b8a2ef1f601d2d2db180"); 			//IPHONE15-X
-      caps.setCapability("udid", "d5fde73de73a52c656c6b6f6ab7bbbb11c39d1ba");				//IPHONE6
+      //caps.setCapability("udid", "d5fde73de73a52c656c6b6f6ab7bbbb11c39d1ba");				//IPHONE6
+      caps.setCapability("udid", "00008020-00111DA63A50003A");							//iPhone # 21
       caps.setCapability("noReset", true);
       caps.setCapability("bundleId","org.safemovement.frontend");
       caps.setCapability("xcodeOrgId", "Q3HXH3MWZ2");
